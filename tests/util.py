5226ea5e31748c32f1fb6143632363c2a40671df
Thomas Isele <t.m.isele@posteo.de>
2018-07-25 12:58:00 +0200

    Comment the needs_gssapi decorator

--- tests/util.py
+++ tests/util.py
@@ -11,6 +11,11 @@ def _support(filename):
 
 # TODO: consider using pytest.importorskip('gssapi') instead? We presumably
 # still need CLI configurability for the Kerberos parameters, though, so can't
+# Using pytest.importorskip doesn't work in the current setup because
+# importing paramiko is done differently when gssapi is there.
+# However, the test decorator imports the module in question ('gssapi') after paramiko
+# has been imported. Thus effectively not testing the logic the import statements in
+# ssh_gss.py are wrapped in (bad practice anyway.)
 # JUST key off presence of GSSAPI optional dependency...
 # TODO: anyway, s/True/os.environ.get('RUN_GSSAPI', False)/ or something.
 needs_gssapi = pytest.mark.skipif(False, reason="No GSSAPI to test")
